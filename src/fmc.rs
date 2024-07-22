3e563f4c2301f0aaed077de3c81672f62251a001
Moritz von Göwels <moritz@tarn-vedra.de>
2024-07-22 11:48:01 +0200

    fix fmc (by parsing JSON from HTML - eww)

--- src/fmc.rs
+++ src/fmc.rs
@@ -1,5 +1,6 @@
 use std::time::Duration;
 
+use anyhow::anyhow;
 use tracing::{info, warn};
 
 pub type Update = serde_json::Value;
@@ -11,7 +12,7 @@ pub fn subscribe(race: &str) -> tokio::sync::watch::Receiver<Update> {
 
     let (tx, rx) = tokio::sync::watch::channel(serde_json::Value::Null);
 
-    let url = format!("https://www.followmychallenge.com/live/{race}/data/ridersArray.json");
+    let url = format!("https://www2.followmychallenge.com/live/{race}/");
 
     tokio::task::spawn(async move {
         loop {
@@ -29,7 +30,16 @@ pub fn subscribe(race: &str) -> tokio::sync::watch::Receiver<Update> {
 }
 
 async fn poll(url: &str) -> Result<serde_json::Value, anyhow::Error> {
-    let json = reqwest::get(url).await?.json::<serde_json::Value>().await?;
+    const PREFIX: &'static str = "var ridersArray = ";
+    let text = reqwest::get(url).await?.text().await?;
+    let line = text
+        .lines()
+        .find_map(|line| line.trim().strip_prefix(PREFIX))
+        .ok_or(anyhow!("Failed to find {PREFIX:?} line"))?;
+
+    let json = serde_json::StreamDeserializer::new(serde_json::de::StrRead::new(line))
+        .next()
+        .ok_or(anyhow!("Failed to parse JSON"))??;
 
     Ok(json)
 }
